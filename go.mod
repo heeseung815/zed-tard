f8a005a76fc0d6b228c792f0cdc219c5ab49f243
Cho, Hee Seung <heeseung815@gmail.com>
2022-04-20 07:41:24 +0900

    add codes

--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module tard
 go 1.17
 
 require (
+	dev.azure.com/carrotins/hdm/hdm-go.git v0.0.0-00010101000000-000000000000
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.3.0
 	github.com/gin-gonic/gin v1.7.7
+	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.8.1
+	github.com/spf13/viper v1.9.0
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.21.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
@@ -21,20 +30,25 @@ require (
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/text v0.3.5 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/ini.v1 v1.63.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
+
+replace dev.azure.com/carrotins/hdm/hdm-go.git => /Users/hscho/workspace/carrot/hdm-go
--- main/tard/start.go
+++ main/tard/start.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 	"tard/mods"
+	"tard/mods/tlog"
 
+	"dev.azure.com/carrotins/hdm/hdm-go.git/httpsvr"
+	"dev.azure.com/carrotins/hdm/hdm-go.git/logging"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var startCmd = func() *cobra.Command {
@@ -23,12 +30,80 @@ var startCmd = func() *cobra.Command {
 	return cmd
 }()
 
+var loggingConfig *logging.Config
+
 func startArgs(cmd *cobra.Command, args []string) error {
+	verbosePrint("verbose: %t", verbose)
+	verbosePrint("configPath: %s", configPath)
+	verbosePrint("pname: %s", viper.GetString("pname"))
+
+	loggingConfig = &logging.Config{
+		Name:               viper.GetString("logger.name"),
+		Console:            viper.GetBool("logger.console"),
+		Filename:           viper.GetString("logger.filename"),
+		Append:             viper.GetBool("logger.append"),
+		MaxSize:            viper.GetInt("logger.maxsize"),
+		MaxBackups:         viper.GetInt("logger.maxbackups"),
+		MaxAge:             viper.GetInt("logger.maxage"),
+		Compress:           viper.GetBool("logger.compress"),
+		DefaultPrefixWidth: viper.GetInt("logger.prefix-width"),
+		DefaultLevel:       logging.ParseLogLevel(viper.GetString("log-level")),
+	}
+
+	for i := range viper.Get("logger.levels").([]interface{}) {
+		prefix := fmt.Sprintf("logger.levels.%d", i)
+		pattern := viper.GetString(prefix + ".pattern")
+		levelName := viper.GetString(prefix + ".level")
+
+		level := logging.ParseLogLevel(levelName)
+
+		lcfg := logging.LevelConfig{Pattern: pattern, Level: level}
+		loggingConfig.Levels = append(loggingConfig.Levels, lcfg)
+	}
 
 	return nil
 }
 
 func startRun(cmd *cobra.Command, args []string) {
+	// Set Logging
+	logging.Configure(loggingConfig)
+
+	log := logging.GetLog("tard-server")
+
+	// Banner
+	log.Info(BootBanner(viper.GetString("pname"), mods.VersionDescription()))
+
+	// GOMAXPROCS
+	if viper.IsSet("GOMAXPROCS") {
+		n := viper.GetInt("GOMAXPROCS")
+		runtime.GOMAXPROCS(n)
+	}
+	log.Infof("GOMAXPROCS: %d", runtime.GOMAXPROCS(0))
+
+	// Service for tlogs
+	service := httpsvr.NewServer(&httpsvr.HttpServerConfig{
+		DisableConsoleColor: false,
+		DebugMode:           false,
+		LoggingConfig: &logging.Config{
+			Name:     "http-tard",
+			Console:  false,
+			Filename: ".", // disable http access logging
+		},
+		LoggerName: "tard-http-server",
+	})
+	httpPrefix := viper.GetString("server.http-prefix")
+	serverBind := viper.GetString("server.bind")
+
+	service.GET(httpPrefix+"/version", handleVersion)
+	service.GET(httpPrefix+"/trips/:tripId/logs", tlog.HandleGetTLog())
+	service.POST(httpPrefix+"/trip/log", tlog.HandlePostTLog())
+	lsnr, err := net.Listen("tcp", serverBind)
+	if err != nil {
+		log.Errorf("listener failed, %s", err)
+	}
+
+	service.Start(lsnr)
+
 	// Wait Signal
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
